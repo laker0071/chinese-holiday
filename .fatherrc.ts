fcc39a80320e90a2b4c659661280b9a698e5c5ce
laker0071 <laker007@live.cn>
2024-03-03 16:33:14 +0800

    add cjs umd

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'father';
 export default defineConfig({
   esm: { input: 'src/client' },
   cjs: { input: 'src/server' },
+  umd: { entry: 'src/client' },
   prebundle: {
     deps: {}
   },
